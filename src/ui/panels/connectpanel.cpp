74ea07f765430b67005584248d0cbe6be07a36be
Asset34 <artemiy.kashintsev@gmail.com>
2018-10-22 20:22:32 +0300

    ConnectPanel: implement class

---
+++ src/ui/panels/connectpanel.cpp
@@ -0,0 +1,26 @@
+#include "connectpanel.hpp"
+
+#include <QVBoxLayout>
+#include <QPushButton>
+
+#include <ui/subpanels/addresssubpanel.hpp>
+
+ConnectPanel::ConnectPanel(QWidget *parent)
+    : QWidget(parent)
+{
+    // Create address subpanel
+    m_addressSubpanel = new AddressSubpanel;
+
+    // Create layout
+    m_layout = new QVBoxLayout;
+    m_layout->setContentsMargins(0, 0, 0, 0);
+    m_layout->addWidget(m_addressSubpanel);
+
+    // Create widget
+    setLayout(m_layout);
+    setContentsMargins(5, 5, 5, 5);
+}
+
+ConnectPanel::~ConnectPanel()
+{
+}
---
+++ src/ui/panels/connectpanel.hpp
@@ -0,0 +1,24 @@
+#ifndef CONNECTPANEL_HPP
+#define CONNECTPANEL_HPP
+
+#include <QWidget>
+
+class QVBoxLayout;
+class AddressSubpanel;
+
+class ConnectPanel : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ConnectPanel(QWidget *parent = nullptr);
+    virtual ~ConnectPanel();
+
+private:
+    QVBoxLayout *m_layout;
+
+    AddressSubpanel *m_addressSubpanel;
+
+};
+
+#endif // CONNECTPANEL_HPP
