11b6bfae9d592156d82c125fee4fad724fac8fb6
Asset34 <artemiy.kashintsev@gmail.com>
2018-10-22 20:15:40 +0300

    AddressSubpanel: implement class

---
+++ src/ui/subpanels/addresssubpanel.cpp
@@ -0,0 +1,49 @@
+#include "addresssubpanel.hpp"
+
+#include <QVBoxLayout>
+#include <QFormLayout>
+#include <QPushButton>
+
+#include <ui/widgets/ipinputbox.hpp>
+
+#include <streamclientmanager.hpp>
+
+AddressSubpanel::AddressSubpanel(QWidget *parent)
+    : QGroupBox(parent)
+{
+    // Create ip input box
+    m_ipInputBox = new IpInputBox;
+
+    // Create address layout
+    m_addressLayout = new QFormLayout;
+    m_addressLayout->setContentsMargins(0, 0, 0, 0);
+    m_addressLayout->addRow("Address:", m_ipInputBox);
+
+    // Create connect button
+    m_connectButton = new QPushButton("Connect");
+
+    // Create main layout
+    m_mainLayout = new QVBoxLayout;
+    m_mainLayout->setContentsMargins(0, 0, 0, 0);
+    m_mainLayout->addLayout(m_addressLayout);
+    m_mainLayout->addWidget(m_connectButton);
+
+    // Create widget
+    setTitle("Address");
+    setContentsMargins(5, 20, 5, 5);
+    setLayout(m_mainLayout);
+
+    // Create connections
+    connect(m_connectButton, &QPushButton::clicked,
+            [this](){
+        StreamClientManager &streamManager = StreamClientManager::getInstance();
+        streamManager.open(
+            m_ipInputBox->getIp(),
+            m_ipInputBox->getPort()
+        );
+    });
+}
+
+AddressSubpanel::~AddressSubpanel()
+{
+}
---
+++ src/ui/subpanels/addresssubpanel.hpp
@@ -0,0 +1,29 @@
+#ifndef ADDRESSSUBPANEL_HPP
+#define ADDRESSSUBPANEL_HPP
+
+#include <QGroupBox>
+
+class QVBoxLayout;
+class QFormLayout;
+class QPushButton;
+class IpInputBox;
+
+class AddressSubpanel : public QGroupBox
+{
+    Q_OBJECT
+
+public:
+    explicit AddressSubpanel(QWidget *parent = nullptr);
+    virtual ~AddressSubpanel();
+
+private:
+    QVBoxLayout *m_mainLayout;
+    QFormLayout *m_addressLayout;
+
+    IpInputBox *m_ipInputBox;
+
+    QPushButton *m_connectButton;
+
+};
+
+#endif // ADDRESSSUBPANEL_HPP
